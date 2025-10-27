12ebc3ce3b4544ad72dd138e02df4030fa58ad29
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-10-27 09:08:51 -0300

    colcando o useState no formulario de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
 import ConteudoPage from "@/components/ConteudoPage";
 import TituloPage from "@/components/TituloPage"
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [sku, setSku] = useState("");
+  const [preco, setPreco] = useState("");
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  function handleEnviar(e: FormEvent) {
+    e.preventDefault()
+    const produto = {
+      sku,
+      preco,
+      nome,
+      descricao
+    }
+    setSku("");
+    setPreco("");
+    setNome("");
+    setDescricao("");
+    console.log(produto);
+  }
+
   return (
     <div className="flex h-screen flex-col w-full">
       <TituloPage titulo="Home"/>
@@ -20,39 +42,53 @@ export default function Home() {
                   type="text"
                   placeholder="Digite o SKU do produto"
                   className="p-2 border-gray-300 border text-gray-400 rounded-sm"
-                
+                  onChange={e => setSku(e.target.value)}
+                  value={sku}
+
                 />
               </div>
               <div className="flex flex-col flex-1 gap-4">
-                <label htmlFor="inputSKU">Preço: *</label>
+                <label htmlFor="inputPreco">Preço: *</label>
                 <input
-                  id="inputSKU"
+                  id="inputPreco"
                   type="number"
                   placeholder="Digite o SKU do produto"
                   className="p-2 border-gray-300 border text-gray-400 rounded-sm"
+                  onChange={e => setPreco(e.target.value)}
+                  value={preco}
                 
                 />
               </div>
             </div>
 
-            <label htmlFor="inputSKU">Nome: *</label>
+            <label htmlFor="inputNome">Nome: *</label>
             <input 
-              id="inputSKU" 
+              id="inputNome" 
               type="text" 
               placeholder="Digite o SKU do produto"
               className="p-2 border-gray-300 border text-gray-400 rounded-sm"
+              onChange={e => setNome(e.target.value)}
+              value={nome}
             
             />
 
-            <label htmlFor="inputSKU">Descrição: *</label>
+            <label htmlFor="inputDescricao">Descrição: *</label>
             <textarea
-              id="inputSKU" 
+              id="inputDescricao" 
               placeholder="Digite o SKU do produto"
               className="p-2 border-gray-300 border text-gray-400 rounded-sm"
+              onChange={e => setDescricao(e.target.value)}
+              value={descricao}
+        
             
             />
 
-            <button className="bg-blue-500 rounded-sm text-white p-2 ">
+            <button 
+              className="bg-blue-500 rounded-sm text-white p-2 "
+              type="button"
+              onClick={handleEnviar}
+                   
+            >
               Salvar
             </button>
 
